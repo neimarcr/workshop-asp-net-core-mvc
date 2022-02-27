dcf21508656403109b4cc05e447e14e49eac6106
Neimar <neimar_cr@hotmail.com.br>
2022-02-27 18:26:37 -0300

    eager loading and details page

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
 ﻿using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services;
 
@@ -24,7 +25,7 @@ public class SellerService
 
     public Seller? FindById(int id)
     {
-        return _context.Seller.FirstOrDefault(x => x.Id == id);
+        return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
     }
 
     public void Remove(int id)
