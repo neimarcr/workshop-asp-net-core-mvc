b1b229c96f2203be1995d21470e479a0492bacf9
Neimar <neimar_cr@hotmail.com.br>
2022-02-27 11:42:55 -0300

    ViewModel SallerForm e select de departments

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext")));
 builder.Services.AddTransient<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
