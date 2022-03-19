b89ac4a0f6fab3c07f85248760eb050b3553b106
Neimar <neimar_cr@hotmail.com.br>
2022-03-19 18:10:44 -0300

    Asynchronous using Task (async, await)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -13,40 +13,41 @@ public class SellerService
         _context = context;
     }
 
-    public List<Seller> FindAll()
+    public async Task<List<Seller>> FindAllAsync()
     {
-        return _context.Seller.ToList();
+        return await _context.Seller.ToListAsync();
     }
 
-    public void Insert(Seller obj)
+    public async Task InsertAsync(Seller obj)
     {
         _context.Add(obj);
-        _context.SaveChanges();
+        await _context.SaveChangesAsync();
     }
 
-    public Seller? FindById(int id)
+    public async Task<Seller?> FindByIdAsync(int id)
     {
-        return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
+        return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(x => x.Id == id);
     }
 
-    public void Remove(int id)
+    public async Task RemoveAsync(int id)
     {
         var obj = _context.Seller.Find(id);
         if (obj != null)
         {
             _context.Seller.Remove(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
     }
 
-    public void Update(Seller seller)
+    public async Task UpdateAsync(Seller seller)
     {
-        if (!_context.Seller.Any(x => x.Id == seller.Id))
+        bool hasAny = await _context.Seller.AnyAsync(x => x.Id == seller.Id);
+        if (!hasAny)
             throw new NotFoundException("Id not found");
         try
         {
             _context.Update(seller);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
         catch (DbUpdateConcurrencyException ex)
         {
